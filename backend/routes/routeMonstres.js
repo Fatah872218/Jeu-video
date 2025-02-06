03c6aca1d120ae30f2ae2cb27939a836acc92abb
CBalthazar <balthclement@gmail.com>
2025-02-06 09:32:00 +0100

    modification routeMonstres

--- backend/routes/routeMonstres.js
+++ backend/routes/routeMonstres.js
@@ -1,10 +1,5 @@
 const express = require("express");
 const router = express.router();
+const controllerMonstres = require("../controllers/controllerMonstres.js");
 
-router.get("/:id");
-
-router.post("/");
-
-router.put("/:id");
-
-router.delete("/:id", controllerPersonnages.supprimerMonstre);
+router.delete("/:id", controllerMonstres.supprimerMonstre);
