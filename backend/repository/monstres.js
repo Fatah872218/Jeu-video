9db97f410977c9ad499ae5dc3560f6cd2cc66438
CBalthazar <balthclement@gmail.com>
2025-02-06 09:59:27 +0100

    fusion repo monstres

--- backend/repository/monstres.js
+++ backend/repository/monstres.js
@@ -26,6 +26,25 @@ class repoMonstres {
       if (conn) conn.release();
     }
   }
+
+  async modifierMonstre(id, changes) {
+    let conn;
+    try {
+      conn = await this.pool.getConnection();
+      const sqlString = toSQL(changes);
+      console.log(`UPDATE Monstres SET ${sqlString} WHERE id = ${id}`);
+      let res = await conn.query(
+        `UPDATE Monstres SET ${sqlString} WHERE id = ?`,
+        [id]
+      );
+      return res;
+    } catch (err) {
+      console.error(err.message);
+      throw err;
+    } finally {
+      if (conn) conn.release();
+    }
+  }
 }
 
 module.exports = new repoMonstres();
