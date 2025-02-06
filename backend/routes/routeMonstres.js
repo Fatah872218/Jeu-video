226ab88d0251120c1a02026f5bfc0ae5d0aa5d15
Balthazar <90700196+CBalthazar@users.noreply.github.com>
2025-02-06 09:57:00 +0100

    Update routeMonstres.js

--- backend/routes/routeMonstres.js
+++ backend/routes/routeMonstres.js
@@ -3,3 +3,4 @@ const router = express.router();
 const controllerMonstres = require("../controllers/controllerMonstres.js");
 
 router.delete("/:id", controllerMonstres.supprimerMonstre);
+router.put("/:id", controllerMonstres.modifierMonstre);
